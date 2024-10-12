6c03b192c4d53587c557cbe6c9c2e3435109541d
AjayThandavamurthy <125175600+AjayThandavamurthy@users.noreply.github.com>
2024-10-12 13:48:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,66 +1,72 @@
 import cv2
-import serial  # if you have not already done so
-import numpy as np
+import serial
+import time
+import imutils
+from cvzone.FaceMeshModule import FaceMeshDetector
+import cvzone
 
-def set_res(cap, x, y):
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, int(x))
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, int(y))
-
-ser = serial.Serial('COM3', 250000)  # change if serial not COM13
-
-cap = cv2.VideoCapture(0)
-
-frame_w = 640
-frame_h = 480
-set_res(cap, frame_w, frame_h)
-
-# Create the haar cascade
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+cap = cv2.VideoCapture(0)
+ArduinoSerial = serial.Serial('COM3', 9600, timeout=0.1)
+time.sleep(1)
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    cap.read()
-    # cv2.imshow('original', frame)
+detector = FaceMeshDetector(maxFaces=1)
+_, start_frame = cap.read()
+start_frame = imutils.resize(start_frame, width=1000)
+start_frame = cv2.cvtColor(start_frame, cv2.COLOR_BGR2GRAY)
+start_frame = cv2.GaussianBlur(start_frame, (21, 21), 0)
 
-    frame = cv2.flip(frame, 1)
-    # cv2.imshow('flipped', frame)
+while cap.isOpened():
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # cv2.imshow('gray', gray)
+    _, frame = cap.read()
+    frame = imutils.resize(frame, width=1000)
 
-    faces = np.array([])
-    faces = face_cascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=20,
-        minSize=(30, 30)
-        # flags = cv2.CV_HAAR_SCALE_IMAGE
-    )
+    frame_bw = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame_bw = cv2.GaussianBlur(frame_bw, (5, 5), 0)
+    difference = cv2.absdiff(frame_bw, start_frame)
+    threshold = cv2.threshold(difference, 25, 255, cv2.THRESH_BINARY)[1]
+    start_frame = frame_bw
 
-    # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    # cv2.imshow("Cam", threshold)
+    if threshold.sum() > 3000000:
+        print("found")
 
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    success, img = cap.read()
+    img = cv2.flip(img, 1)  # Mirror the image
+
+    # Face detection
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 6)  # Detect faces
+    for x, y, w, h in faces:
+        # Sending coordinates to Arduino
+        string = 'X{0:d}Y{1:d}'.format((x + w // 2), (y + h // 2))
+        print(string)
+        ArduinoSerial.write(string.encode('utf-8'))
+        # Plot the region of interest (ROI)
+        cv2.rectangle(img, (x-80, y-80), (x + 100 + w, y + 150 + h), (0, 0, 255), 3)
+        #cv2.line(img, (0,y+100),(1280,y+100),(0,0,0),2)
+
+    img, faces = detector.findFaceMesh(img, draw=False)
+    if faces:
+        face = faces[0]
+        pointLeft = face[145]
+        pointRight = face[374]
+
+        # Estimating depth
+        w, _ = detector.findDistance(pointLeft, pointRight)
+        W = 6.3  # Assuming a constant width for the reference object in the frame
+        f = 520  # Focal length of your camera, adjust accordingly
+        d = (W * f) / w
+        print("Distance:", d)
+        cv2.putText(img, "Person Found", (400, 450), cv2.FONT_ITALIC, 1, (0, 0, 255), 2 )
+
+        # Displaying depth on the image
+        cvzone.putTextRect(img, f"Distance: {int(d)} cm", (face[10][0] - 120, face[10][1] - (-300)), scale=1.5)
+
+    cv2.imshow('img', img)
+
+    if cv2.waitKey(10) & 0xFF == ord('q'):
         break
 
-    if ([i for i in faces]):
-        face_center_x = faces[0, 0] + faces[0, 2] / 2
-        face_center_y = faces[0, 1] + faces[0, 3] / 2
-        # print(faces)
-        err_x = 30 * (face_center_x - frame_w/2) / (frame_w/2)
-        err_y = 30 * (face_center_y - frame_h/2) / (frame_h/2)
-        ser.write((str(err_x) + "x!").encode())
-        ser.write((str(err_y) + "y!").encode())
-        print("X: ", err_x, " ", "Y: ", err_y)
-    else:
-        ser.write("o!".encode())
-
-# When everything done, release the capture
-ser.close()
 cap.release()
 cv2.destroyAllWindows()
